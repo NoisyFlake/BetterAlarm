23c1907399d7a393f9d46c76baccd13984809d6c
NoisyFlake <noisyflake@icloud.com>
2023-04-24 21:34:24 +0200

    Replace overly complicated dpkg call with PACKAGE_VERSION

--- preferences/BetterAlarmRootListController.m
+++ preferences/BetterAlarmRootListController.m
@@ -61,40 +61,11 @@
 	version.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:13.0f];
 	version.textColor = UIColor.systemGrayColor;
 	version.textAlignment = NSTextAlignmentCenter;
-	version.text = @"Version unknown";
-	version.alpha = 0;
+	version.text = [NSString stringWithFormat:@"Version %@", PACKAGE_VERSION];
 
 	[header addSubview:tweakName];
 	[header addSubview:version];
 	[self.table setTableHeaderView:header];
-
-	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-		NSPipe *pipe = [NSPipe new];
-
-		NSTask *task = [NSTask new];
-		task.arguments = @[@"--showformat=${Version}", @"--show", @"com.noisyflake.betteralarm"];
-		task.launchPath = ROOT_PATH_NS_VAR(@"/usr/bin/dpkg-query");
-		task.standardOutput = pipe;
-		[task launch];
-		[task waitUntilExit];
-
-		NSFileHandle *file = [pipe fileHandleForReading];
-		NSData *output = [file readDataToEndOfFile];
-		NSString *outputString = [[NSString alloc] initWithData:output encoding:NSUTF8StringEncoding];
-		outputString = [outputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-		[file closeFile];
-
-		dispatch_async(dispatch_get_main_queue(), ^(void){
-			// Update label on the main queue
-			if ([outputString length] > 0) {
-				version.text = [NSString stringWithFormat:@"Version %@", outputString];
-			}
-
-			[UIView animateWithDuration:0.3 delay:0 options:UIViewAnimationOptionCurveEaseIn animations:^{
-				version.alpha = 1;
-			} completion:nil];
-		});
-	});
 }
 
 @end
